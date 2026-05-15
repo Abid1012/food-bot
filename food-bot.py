cc20b1d8711a8e3dad234de8ed1b3c9f8a170cb2
Abid1012 <anjumabid2006@gmail.com>
2026-05-15 23:53:57 +0600

    Update food-bot.py

--- food-bot.py
+++ food-bot.py
@@ -1,27 +1,15 @@
-import asyncio
-import schedule
-import time
-from telegram import Bot
-
-TOKEN = "8729316019:AAGEvWCpxsSkT5Zc1Xw44z1ZCH-KWBcg2cE"
-GROUP_ID = "-1003934516101"
-
-bot = Bot(token=TOKEN)
-
-async def send_food_question():
-    await bot.send_message(
-        chat_id=GROUP_ID,
-        text="🍽 আগামীকাল বুয়া কি রান্না করবে?"
-    )
-
-def job():
-    asyncio.run(send_food_question())
-
-# Bangladesh 11.50 PM = 23:50 UTC
-schedule.every().day.at("23:50").do(job)
-
-print("Bot running...")
-
-while True:
-    schedule.run_pending()
-    time.sleep(30)
+‎import asyncio
+‎from telegram import Bot
+‎
+‎TOKEN = "8729316019:AAGEvWCpxsSkT5Zc1Xw44z1ZCH-KWBcg2cE"
+‎GROUP_ID = "-1003934516101"
+‎
+‎bot = Bot(token=TOKEN)
+‎
+‎async def main():
+‎    await bot.send_message(
+‎        chat_id=GROUP_ID,
+‎        text="✅ BOT WORKING!"
+‎    )
+‎
+‎asyncio.run(main())
