4e0fe8be1ed587d4267f14de16302831a2c2a28c
Abid1012 <anjumabid2006@gmail.com>
2026-05-15 12:52:20 +0600

    Add food-bot.py script

---
+++ food-bot.py
@@ -0,0 +1,27 @@
+import asyncio
+import schedule
+import time
+from telegram import Bot
+
+TOKEN = "8729316019:AAGEvWCpxsSkT5Zc1Xw44z1ZCH-KWBcg2cE"
+GROUP_ID = "1003934516101"
+
+bot = Bot(token=TOKEN)
+
+async def send_food_question():
+    await bot.send_message(
+        chat_id=GROUP_ID,
+        text="🍽 What do you want to eat tomorrow?"
+    )
+
+def job():
+    asyncio.run(send_food_question())
+
+# Bangladesh 10 PM = 16:00 UTC
+schedule.every().day.at("16:00").do(job)
+
+print("Bot running...")
+
+while True:
+    schedule.run_pending()
+    time.sleep(30)
